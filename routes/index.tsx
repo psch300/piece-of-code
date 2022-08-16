d445b27b232320915e1d4b747d57e1ac886ff335
Sungchang Park <psch300@gmail.com>
2022-08-17 00:40:51 +0900

    [feat] add title tag to index routes

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,17 +1,25 @@
 /** @jsx h */
-import { h } from 'preact';
+import { Fragment, h } from 'preact';
 import { tw } from '@twind';
 import ComingSoon from '../islands/ComingSoon.tsx';
+import { Head } from '$fresh/runtime.ts';
+
+const TITLE = 'piece of code - bring code pieces in my mind to reality';
 
 const Home = () => {
   return (
-    <div class={tw`bg-black h-screen w-screen`}>
-      <div
-        class={tw`flex items-center justify-center text-white h-full w-full`}
-      >
-        <ComingSoon />
+    <Fragment>
+      <Head>
+        <title>{TITLE}</title>
+      </Head>
+      <div class={tw`bg-black h-screen w-screen`}>
+        <div
+          class={tw`flex items-center justify-center text-white h-full w-full`}
+        >
+          <ComingSoon />
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
